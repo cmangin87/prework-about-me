397897216d805cc060a33bbf09998f71217b965b
cmangin87 <49618564+cmangin87@users.noreply.github.com>
2019-09-15 20:32:17 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+document.getElementById("growBtn").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("blueBtn").addEventListener("click", function(){
+
+    document.getElementById("box").color = "p {color: blue;}";
+
+});
+
+document.getElementById("fadeBtn").addEventListener("click", function(){
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+});
+
+document.getElementById("resetBtn").addEventListener("click", function(){
+
+    document.getElementById("box")./* CSS reset */
+
+
+    document.getElementById("box").style.height = "150px";
+
+});
